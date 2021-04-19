8a853289a2bfb1f93ee750a0a7c247b7e4478a03
Alec Lang <53492781+lostlang@users.noreply.github.com>
2021-04-19 14:05:40 +0300

    Add image sampler

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import fnmatch
 import shutil
 import random
+from PIL import Image, ImageOps
 
 
 def unzip_data(name, path_files):
@@ -56,7 +57,6 @@ def reorganize_data_set(path_to_dataset, name_tags):
         else:
             shutil.copy("/".join([path_to_dataset, file_name]),
                         "/".join([dir_dataset, dir_train, tag]))
-        pass
 
 
 def split_to_train_test(tags_and_amount, percent):
@@ -93,6 +93,75 @@ def split_to_train_test(tags_and_amount, percent):
                     file.write(text)
 
 
+def normalize_train_data_and_generate_new_sample(tags_and_amount, need_sample, need_image_size):
+    dir_dataset = "dataset"
+    dir_train = "train"
+    dir_normalize = "normal"
+    new_dir = "/".join([dir_dataset, dir_normalize])
+
+    try:
+        os.mkdir(new_dir)
+    except FileExistsError:
+        pass
+
+    for tag in tags_and_amount.keys():
+        file_list = os.listdir("/".join([dir_dataset, dir_train, tag]))
+        with open("/".join([dir_dataset, dir_train, tag]) + ".txt", "r") as all_info:
+            i = 0
+            for line in all_info:
+                image_info = line.strip().split()
+                if fnmatch.fnmatch(file_list[i].split(".")[0], f'*_{image_info[0]}'):
+                    i += 1
+                    image_name = "/".join([dir_dataset, dir_train, tag, file_list[i]])
+                    if tags_and_amount[tag] / need_sample > 1:
+                        new_image(image_name, new_dir, need_image_size, 1)
+                    elif tags_and_amount[tag] / need_sample > 0.75:
+                        new_image(image_name, new_dir, need_image_size, 2)
+                    elif tags_and_amount[tag] / need_sample > 0.5:
+                        new_image(image_name, new_dir, need_image_size, 3)
+                    else:
+                        new_image(image_name, new_dir, need_image_size, 4)
+                    break
+            break
+
+
+def new_image(old_name, path_to_new, new_size, amount):
+    image = Image.open(old_name)
+    bw = Image.new("L", (new_size, new_size))
+    bw.paste(image.resize((round(new_size * image.size[0] / max(image.size)),
+                           round(new_size * image.size[1] / max(image.size)))))
+    name = "sample_{}.{}"
+    new_name = name.format(len(os.listdir(path_to_new)), old_name.split(".")[-1])
+    bw.save("/".join([path_to_new, new_name]))
+
+    bw_mirror = ImageOps.mirror(bw)
+
+    new_name = name.format(len(os.listdir(path_to_new)), old_name.split(".")[-1])
+    bw_mirror.save("/".join([path_to_new, new_name]))
+
+    if amount > 1:
+        bw_r5 = bw.rotate(5)
+        bw_mirror_r5 = bw_mirror.rotate(5)
+        new_name = name.format(len(os.listdir(path_to_new)), old_name.split(".")[-1])
+        bw_r5.save("/".join([path_to_new, new_name]))
+
+        new_name = name.format(len(os.listdir(path_to_new)), old_name.split(".")[-1])
+        bw_mirror_r5.save("/".join([path_to_new, new_name]))
+
+    if amount > 2:
+        new_name = name.format(len(os.listdir(path_to_new)), old_name.split(".")[-1])
+        bw.rotate(-5).save("/".join([path_to_new, new_name]))
+
+        new_name = name.format(len(os.listdir(path_to_new)), old_name.split(".")[-1])
+        bw_mirror.rotate(-5).save("/".join([path_to_new, new_name]))
+    if amount > 3:
+        new_name = name.format(len(os.listdir(path_to_new)), old_name.split(".")[-1])
+        bw_r5.rotate(-5).save("/".join([path_to_new, new_name]))
+
+        new_name = name.format(len(os.listdir(path_to_new)), old_name.split(".")[-1])
+        bw_mirror_r5.rotate(-5).save("/".join([path_to_new, new_name]))
+
+
 if __name__ == "__main__":
     name_dataset = "cats_dogs_dataset.tar"
     path_dataset = name_dataset.split(".")[0]
@@ -106,4 +175,8 @@ if __name__ == "__main__":
 
     #split_to_train_test(animal_amount, test_percent)
 
-    
+    animal_amount = {tag: round(animal_amount[tag] * 0.9) for tag in animal_amount.keys()}
+
+    normalize_train_data_and_generate_new_sample(animal_amount, 200,  200)
+
+
