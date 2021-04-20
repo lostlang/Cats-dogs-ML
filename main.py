86bd48a200f5690d4137b58580dbffcc981a8c90
Alec Lang <53492781+lostlang@users.noreply.github.com>
2021-04-20 17:54:31 +0300

    Add rotation face rectangle (bug)

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import fnmatch
 import shutil
 import random
 from PIL import Image, ImageOps
+import numpy
+import math
+import matplotlib.pyplot as plt
 
 
 def unzip_data(name, path_files):
@@ -109,24 +112,21 @@ def normalize_train_data_and_generate_new_sample(tags_and_amount, need_sample, n
         with open("/".join([dir_dataset, dir_train, tag]) + ".txt", "r") as all_info:
             i = 0
             for line in all_info:
-                image_info = line.strip().split()
-                if fnmatch.fnmatch(file_list[i].split(".")[0], f'*_{image_info[0]}'):
-                    i += 1
+                image_info = list(map(int, line.strip().split()))
+                if i < len(file_list) and fnmatch.fnmatch(file_list[i].split(".")[0], f'*_{image_info[0]}'):
                     image_name = "/".join([dir_dataset, dir_train, tag, file_list[i]])
-                    if tags_and_amount[tag] / need_sample > 1:
-                        new_image(image_name, new_dir, need_image_size, 1)
-                    elif tags_and_amount[tag] / need_sample > 0.75:
-                        new_image(image_name, new_dir, need_image_size, 2)
-                    elif tags_and_amount[tag] / need_sample > 0.5:
-                        new_image(image_name, new_dir, need_image_size, 3)
-                    else:
-                        new_image(image_name, new_dir, need_image_size, 4)
+                    new_date = new_image(image_name, new_dir, need_image_size, 8, image_info[2:])
+                    for i2 in new_date:
+                        print((i2[0]-i2[2]) * (i2[1]-i2[3]), i2)
+                    i += 1
                     break
             break
 
 
-def new_image(old_name, path_to_new, new_size, amount):
+def new_image(old_name, path_to_new, new_size, amount, info):
+    angle = random.randint(5, 10)
     image = Image.open(old_name)
+
     bw = Image.new("L", (new_size, new_size))
     bw.paste(image.resize((round(new_size * image.size[0] / max(image.size)),
                            round(new_size * image.size[1] / max(image.size)))))
@@ -134,38 +134,91 @@ def new_image(old_name, path_to_new, new_size, amount):
     new_name = name.format(len(os.listdir(path_to_new)), old_name.split(".")[-1])
     bw.save("/".join([path_to_new, new_name]))
 
-    bw_mirror = ImageOps.mirror(bw)
+    new_info = [
+        round(info[0] * new_size / max(image.size), 2),
+        round(info[1] * new_size / max(image.size), 2),
+        round(info[2] * new_size / max(image.size), 2),
+        round(info[3] * new_size / max(image.size), 2)
+    ]
 
-    new_name = name.format(len(os.listdir(path_to_new)), old_name.split(".")[-1])
-    bw_mirror.save("/".join([path_to_new, new_name]))
+    all_image_info = list()
+    all_image_info.append(new_info)
 
     if amount > 1:
-        bw_r5 = bw.rotate(5)
-        bw_mirror_r5 = bw_mirror.rotate(5)
+        bw_mirror = ImageOps.mirror(bw)
+        new_name = name.format(len(os.listdir(path_to_new)), old_name.split(".")[-1])
+        bw_mirror.save("/".join([path_to_new, new_name]))
+        flip = [
+            round(new_size - new_info[2], 2),
+            new_info[1],
+            round(new_size - new_info[0], 2),
+            new_info[3],
+        ]
+        all_image_info.append(flip)
+    if amount > 2:
+        bw_r5 = bw.rotate(angle)
         new_name = name.format(len(os.listdir(path_to_new)), old_name.split(".")[-1])
         bw_r5.save("/".join([path_to_new, new_name]))
-
+        rotate = rotate_rectangle(new_info, angle, new_size / 2)
+        all_image_info.append(rotate)
+    if amount > 3:
+        bw_mirror_r5 = bw_mirror.rotate(angle)
         new_name = name.format(len(os.listdir(path_to_new)), old_name.split(".")[-1])
         bw_mirror_r5.save("/".join([path_to_new, new_name]))
+        rotate = rotate_rectangle(flip, angle, new_size / 2)
+        all_image_info.append(rotate)
+    if amount > 4:
+        new_name = name.format(len(os.listdir(path_to_new)), old_name.split(".")[-1])
+        bw.rotate(-angle).save("/".join([path_to_new, new_name]))
+        rotate = rotate_rectangle(new_info, -angle, new_size / 2)
+        all_image_info.append(rotate)
+    if amount > 5:
+        new_name = name.format(len(os.listdir(path_to_new)), old_name.split(".")[-1])
+        bw_mirror.rotate(-angle).save("/".join([path_to_new, new_name]))
+        rotate = rotate_rectangle(flip, -angle, new_size / 2)
+        all_image_info.append(rotate)
+    if amount > 6:
+        new_name = name.format(len(os.listdir(path_to_new)), old_name.split(".")[-1])
+        bw_r5.rotate(-angle).save("/".join([path_to_new, new_name]))
+        all_image_info.append(new_info)
+    if amount > 7:
+        new_name = name.format(len(os.listdir(path_to_new)), old_name.split(".")[-1])
+        bw_mirror_r5.rotate(-angle).save("/".join([path_to_new, new_name]))
+        all_image_info.append(flip)
 
-    if amount > 2:
-        new_name = name.format(len(os.listdir(path_to_new)), old_name.split(".")[-1])
-        bw.rotate(-5).save("/".join([path_to_new, new_name]))
+    return all_image_info
 
-        new_name = name.format(len(os.listdir(path_to_new)), old_name.split(".")[-1])
-        bw_mirror.rotate(-5).save("/".join([path_to_new, new_name]))
-    if amount > 3:
-        new_name = name.format(len(os.listdir(path_to_new)), old_name.split(".")[-1])
-        bw_r5.rotate(-5).save("/".join([path_to_new, new_name]))
 
-        new_name = name.format(len(os.listdir(path_to_new)), old_name.split(".")[-1])
-        bw_mirror_r5.rotate(-5).save("/".join([path_to_new, new_name]))
+def rotate_rectangle(rectangle, angle, center):
+    all_point = numpy.array([
+        [rectangle[0] - center, rectangle[0] - center],
+        [rectangle[0] - center, rectangle[3] - center],
+        [rectangle[2] - center, rectangle[0] - center],
+        [rectangle[2] - center, rectangle[3] - center]
+    ])
+
+    angle *= math.pi / 180
+    rotate_array = numpy.array([
+        [math.cos(angle), -math.sin(angle)],
+        [math.sin(angle), math.cos(angle)]
+    ])
+
+    rotate_point = numpy.dot(all_point, rotate_array)
+    rotate_point += center
+    new_rectangle = [
+        round(min(rotate_point[:, 0]), 2),
+        round(min(rotate_point[:, 1]), 2),
+        round(max(rotate_point[:, 0]), 2),
+        round(max(rotate_point[:, 1]), 2)
+    ]
+    return new_rectangle
 
 
 if __name__ == "__main__":
     name_dataset = "cats_dogs_dataset.tar"
     path_dataset = name_dataset.split(".")[0]
     test_percent = 10
+
 
     #unzip_data(name_dataset, path_dataset)
 
@@ -176,7 +229,8 @@ if __name__ == "__main__":
     #split_to_train_test(animal_amount, test_percent)
 
     animal_amount = {tag: round(animal_amount[tag] * 0.9) for tag in animal_amount.keys()}
+    tag_sample = max(animal_amount.values())
 
-    normalize_train_data_and_generate_new_sample(animal_amount, 200,  200)
+    normalize_train_data_and_generate_new_sample(animal_amount, tag_sample,  200)
 
 
